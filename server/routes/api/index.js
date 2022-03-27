c8290adb78f4f0263a85d5e6a808c46fa03c50f8
Optical23 <aaron@thefergusons.com>
2022-03-27 13:41:45 -0500

    actually using graphql

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const storeRoutes = require('./store-routes');
-
-router.use('/user', userRoutes);
-router.use('/store', storeRoutes);
-
-module.exports = router;
