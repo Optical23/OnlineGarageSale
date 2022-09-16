a30408d2e3fed7fc35b7aab60ab319f9671be707
Optical23 <aaron@thefergusons.com>
2022-09-16 12:53:14 -0500

    new debug issue

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
 
+const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -28,13 +27,15 @@ app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
+};
+
+app.get('*', (req, res) => {
      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
