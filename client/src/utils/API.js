fa657285d4309ba22dd929235ceb4a9930fc660f
Optical23 <aaron@thefergusons.com>
2022-09-29 21:16:03 -0500

    adding front end api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,3 +17,16 @@ export const createUser = (userData) => {
     });
 };
 
+export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+};
+
+// export const createStore = (storeData) => {
+//     return fetch('/api/')
+// }
